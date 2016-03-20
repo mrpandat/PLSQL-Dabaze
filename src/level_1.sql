afd963ca270295f3e7758c614c4c454f6a702777
pandat <mrpandat@hotmail.fr>
2016-03-20 16:20:25 +0100

    feat(level1): add_zone

--- src/level_1.sql
+++ src/level_1.sql
@@ -6,11 +6,58 @@ add_transport_type(
 	avg_interval INT)
 RETURNS BOOLEAN AS $$ begin
 
-insert into transport(code, name, capacity, avg_interval)
-values(code, name, capacity, avg_interval);
+	insert into transport(code, name, capacity, avg_interval)
+	values(code, name, capacity, avg_interval);
+
 return true;
 
 EXCEPTION
     WHEN others THEN
         RETURN false;
+end;$$ language plpgsql;
+
+Create or replace function 
+add_zone(
+	name VARCHAR(32),
+	price FLOAT)
+returns boolean
+
+AS $$ begin
+
+	insert into zone(price, name)
+	values(price, name);
+
+	return true;
+	EXCEPTION
+		WHEN others THEN
+        		RETURN false;
+
+end; $$ language plpgsql;
+
+Create or replace function 
+add_station(
+	id int,
+	name varchar(64),
+	town varchar(32),
+	zone int,
+	type varchar(3)
+)
+AS $$ begin
+_
+EXCEPTION
+    WHEN others THEN
+        RETURN false;
 end; $$ language plpgsql
+
+
+/****
+Create or replace function 
+
+AS $$ begin
+
+EXCEPTION
+    WHEN others THEN
+        RETURN false;
+end; $$ language plpgsql
+
+**/
--- tests/tests.sql
+++ tests/tests.sql
@@ -1,1 +1,5 @@
 select add_transport_type('TMW','tramway',100,2);
+select add_transport_type('BUS','bus',50,4);
+select add_transport_type('RER','train',500,5);
+select add_transport_type('MTR','metro',300,3);
+
