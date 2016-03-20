fc481023bb79e8facd0a77e975b2586048ace2d4
pandat <mrpandat@hotmail.fr>
2016-03-20 14:48:02 +0100

    feat(create): creating db ok

--- src/init_database.sql
+++ src/init_database.sql
@@ -2,8 +2,9 @@
 
 /* sure */
 
+drop table if exists subscription cascade;
 drop table if exists journey cascade;
-drop table if exists station_line cascade;
+/* drop table if exists station_line cascade; */
 drop table if exists station cascade;
 drop table if exists line cascade;
 drop table if exists transport cascade;
@@ -13,14 +14,14 @@ drop table if exists employee cascade;
 drop table if exists customer cascade;
 drop table if exists town_zipcode cascade;
 drop table if exists town cascade;
-/* end */
+drop table if exists offer cascade;
+drop table if exists zone cascade;
 
 drop table if exists zipcode cascade;
 drop table if exists service cascade;
-drop table if exists subscription cascade;
 drop table if exists subscription_status cascade;
-drop table if exists offer cascade;
-drop table if exists fare_zone cascade;
+
+
 
 CREATE TABLE transport(
     id serial not null primary key,
@@ -40,12 +41,19 @@ CREATE TABLE town(
     name varchar(64) not null
 );
 
+CREATE TABLE zone(
+    id serial not null primary key,
+    price float,
+    name varchar(32)
+);
+
 CREATE TABLE station(
     id serial not null primary key,
     name varchar(64) not null,
     transport_id serial references transport(id),
     line_id serial references line(id),
-    town_id serial references town(id)
+    town_id serial references town(id),
+    zone_id serial references zone(id)
 );
 
 CREATE TABLE customer(
@@ -91,24 +99,30 @@ CREATE TABLE employee(
     customer_id serial references customer(id)
 );
 
-CREATE TABLE contract(
-    id serial not null primary key,
-    departure date,
-    hire_date date not null,
-    employee_id serial references employee(id)
-);
-
-
-/*************** *************/
 CREATE TABLE service(
     id serial not null primary key,
     name varchar(32),
     discount int
 );
 
-CREATE TABLE subscription(
+
+CREATE TABLE contract(
     id serial not null primary key,
-    begin date
+    departure date,
+    hire_date date not null,
+    employee_id serial references employee(id),
+    service_id serial references service(id)
+);
+
+
+CREATE TABLE offer(
+    id serial not null primary key,
+    code char(5),
+    name varchar(32),
+    price int,
+    duration int,
+    highest_zone_id serial references zone(id),
+    lowest_zone_id serial references zone(id)
 );
 
 CREATE TABLE subscription_status(
@@ -116,23 +130,18 @@ CREATE TABLE subscription_status(
     name varchar(32)
 );
 
-CREATE TABLE offer(
+CREATE TABLE subscription(
     id serial not null primary key,
-    code char(5),
-    name varchar(32),
-    price int,
-    duration int
+    begin date,
+    status_id serial references subscription_status(id),
+    customer_id serial references customer(id),
+    offer_id serial references offer(id)
 );
 
-CREATE TABLE fare_zone(
-    id serial not null primary key,
-    price float,
-    name varchar(32)
-);
-
+/*
 CREATE TABLE station_line(
     id serial not null primary key,
     id_station serial references station(id),
     id_line serial references line(id)
 );
-
+*/
